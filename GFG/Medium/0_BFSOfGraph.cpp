1631074a1f18f4430faf0ff12d01323e093b1668
NIRAJ <nirajbhayera4@gmail.com>
2026-08-02 16:43:55 +0530

    Sync GFG - BFS of graph (Medium)

---
+++ GFG/Medium/0_BFSOfGraph.cpp
@@ -0,0 +1,37 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // code here
+        int n=adj.size();
+        vector<int> vis(n,0);
+        
+        vis[0]=1;
+        queue<int> q;
+        q.push(0);
+        vector<int> bfs;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            bfs.push_back(node);
+            
+            
+            for(auto it : adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
+                    
+                }
+            }
+        }
+        return bfs;
+    }
+};
