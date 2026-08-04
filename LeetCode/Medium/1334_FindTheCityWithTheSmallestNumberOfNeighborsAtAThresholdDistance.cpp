d4860163e5283fea6601ef588ae8ff5dc3268417
NIRAJ <nirajbhayera4@gmail.com>
2026-08-04 20:36:32 +0530

    Sync LeetCode - 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance (Medium)

--- LeetCode/Medium/1334_FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance.cpp
+++ LeetCode/Medium/1334_FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance.cpp
@@ -10,11 +10,12 @@ using namespace std;
 class Solution {
 public:
 #define p pair<int, int>
-void dij(int n,unordered_map<int , vector<p>> &adj,vector<int> &result, int s){
-    priority_queue<p, vector<p> , greater<p>>pq;
+void dij(int n , unordered_map<int, vector<p>> adj, vector<int> &result, int s){
+    priority_queue<p , vector<p> , greater<p>>pq;
     pq.push({0,s});
-    // result vector ko fill kr denge INT_MAX  se 
     fill(result.begin(), result.end(), INT_MAX);
+
+
     result[s]=0;
     while(!pq.empty()){
         int d=pq.top().first;
@@ -22,56 +23,54 @@ void dij(int n,unordered_map<int , vector<p>> &adj,vector<int> &result, int s){
         pq.pop();
         for(auto & v : adj[node]){
             int adjnode=v.first;
-            int wt=v.second;
-            if(d + wt < result[adjnode]){
-                result[adjnode]=d+wt;
-                pq.push({d+wt, adjnode});
+            int dist=v.second;
+            if(d + dist < result[adjnode]){
+                result[adjnode]=d +dist;
+                pq.push({d+dist, adjnode});
             }
         }
     }
-
+    
 }
-// esi city find krenge jiska distance threshold se kam ko
-
-int findcity(int n , vector<vector<int>> &SPM,int D){
+int findcity(int n, vector<vector<int>>&SPM, int D){
     int resultcity=-1;
     int leastcount=INT_MAX;
     for(int i=0;i<n;i++){
         int countreach=0;
-
         for(int j=0;j<n;j++){
-            if(i !=j && SPM[i][j] <=D){
+            if(1!=j && SPM[i][j]<=D){
                 countreach++;
-                
             }
         }
         if(countreach <=leastcount){
             leastcount=countreach;
             resultcity=i;
         }
-    }return resultcity;
+    }
+    return resultcity;
 }
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> SPM(n, vector<int> (n,1e9 +7));
+        // vector banayenge jisme fill krenge 
+        vector<vector<int>> SPM( n, vector<int> (n ,1e9+7));
         for(int i=0;i<n;i++){
-            SPM[i][i]=0;// diagonal elements ko fill kr dega 
+            SPM[i][i]=0; // diagonals elements 0 se fill kr denge 
 
         }
-        // adj list -> because weighted and birectional graph hai 
-        unordered_map<int , vector<p>> adj;
+        unordered_map<int, vector<p>> adj;
         for(auto &edge : edges){
             int u=edge[0];
             int v=edge[1];
-            int wt= edge[2];
+            int wt=edge[2];
 
             adj[u].push_back({v, wt});
             adj[v].push_back({u, wt});
             
-        }
-        // har ke node ke liye dijsktra call krega and SPM matrix me bhar dega 
-        for(int i=0;i < n;i++){
-            dij(n, adj, SPM[i], i);
 
+        }
+
+        // har ek index me DIJKSTRA's call kr denge 
+        for(int i=0;i<n;i++){
+            dij(n, adj, SPM[i], i);
         }
         return findcity(n, SPM, distanceThreshold);
 
