fcb19ce5184d79480bbb307f312adf18ae745337
NIRAJ <nirajbhayera4@gmail.com>
2026-08-06 19:42:19 +0530

    Sync LeetCode - 1584. Min Cost to Connect All Points (Medium)

--- LeetCode/Medium/1584_MinCostToConnectAllPoints.cpp
+++ LeetCode/Medium/1584_MinCostToConnectAllPoints.cpp
@@ -9,54 +9,98 @@ using namespace std;
 
 class Solution {
 public:
-typedef pair<int, int>p;
-int PrimsAlgo(vector<vector<p>>&adj,int V){
-    priority_queue<p, vector<p> , greater<p>>pq;
-    pq.push({0,0});
-    vector<bool> visited(V, false);
-    int sum=0;
-    while(!pq.empty()){
-        auto p=pq.top();
-        pq.pop();
-        int wt=p.first;
-        int node=p.second;
-        if(visited[node]==true)continue;
-        visited[node]=true;
-        sum+=wt;
-        for(auto &v : adj[node]){
-            int neigh=v.first;
-            int n_wt=v.second;
-            if(visited[neigh]==false){
-                pq.push({n_wt,neigh});
+    //typedef pair<int, int> p;
+    /**
+    int PrimsAlgo(vector<vector<p>>&adj,int V){
+        priority_queue<p, vector<p> , greater<p>>pq;
+        pq.push({0,0});
+        vector<bool> visited(V, false);
+        int sum=0;
+        while(!pq.empty()){
+            auto p=pq.top();
+            pq.pop();
+            int wt=p.first;
+            int node=p.second;
+            if(visited[node]==true)continue;
+            visited[node]=true;
+            sum+=wt;
+            for(auto &v : adj[node]){
+                int neigh=v.first;
+                int n_wt=v.second;
+                if(visited[neigh]==false){
+                    pq.push({n_wt,neigh});
+                }
             }
+
+
         }
-        
-        
+        return sum;
     }
-    return sum;
-}
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int V=points.size();
-        vector<vector<p>> adj(V);
-        for(int i=0;i <V;i++){
-            for(int j=i+1;j<V;j++){
-                int x1=points[i][0];
-                int y1=points[i][1];
-                
-                int x2=points[j][0];
-                int y2=points[j][1];
-                int d=abs(x1-x2) + abs(y1-y2);
-                adj[i].push_back({j,d});
-                adj[j].push_back({i,d});
-                
+    */
+    vector<int> parent, rank;
+    int find(int i) {
+        if (parent[i] == i)
+            return i;
+        return parent[i] = find(parent[i]);
+    }
+    void Union(int x, int y) {
+        int xp = find(x);
+        int yp = find(y);
+        if (xp == yp)
+            return;
+        if (rank[xp] > rank[yp]) {
+            parent[yp] = xp;
 
-
-                
-
-            }
-            
+        } else if (rank[xp] < rank[yp]) {
+            parent[xp] = yp;
+        } else {
+            parent[xp] = yp;
+            rank[yp]++;
         }
-        return PrimsAlgo(adj, V);
+    }
+
+    int kruskuls(vector<vector<int>>& adj) {
+        int sum = 0;
+        for (auto& temp : adj) {
+            ;
+
+            int u = temp[0];
+            int v = temp[1];
+            int wt = temp[2];
+            int parent_u = find(u);
+            int parent_v = find(v);
+            if (parent_u != parent_v) {
+                Union(u, v);
+                sum += wt;
+            }
+        }
+        return sum;
+    }
+    int minCostConnectPoints(vector<vector<int>>& points) {
+
+        int V = points.size();
+        parent.resize(V);
+        rank.resize(V, 0);
+        for (int i = 0; i < V; i++) {
+            parent[i] = i;
+        }
+        vector<vector<int>> adj;
+        for (int i = 0; i < V; i++) {
+            for (int j = i + 1; j < V; j++) {
+                int x1 = points[i][0];
+                int y1 = points[i][1];
+
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                int d = abs(x1 - x2) + abs(y1 - y2);
+                adj.push_back({i, j, d});
+            }
+        }
+        auto comprator = [&](vector<int>& v1, vector<int>& v2) {
+            return v1[2] < v2[2];
+        };
+        sort(begin(adj), end(adj), comprator);
+        return kruskuls(adj);
     }
 };
 
