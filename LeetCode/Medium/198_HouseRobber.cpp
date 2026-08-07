a3c8b5a44679c3c97ed4bb20f27c50245c027578
NIRAJ <nirajbhayera4@gmail.com>
2026-08-07 19:36:22 +0530

    Sync LeetCode - 198. House Robber (Medium)

--- LeetCode/Medium/198_HouseRobber.cpp
+++ LeetCode/Medium/198_HouseRobber.cpp
@@ -14,9 +14,26 @@ public:
         int prev1=0; // dp[i-1];
 
         for(int num: nums){
-            int curr=max(prev1, prev2 + num);
-            prev2=prev1;
-            prev1=curr;
+            int curr=max(prev1, prev2 + num); // curr= max(0,0+1)=1
+        
+            prev2=prev1;// prev2=0;
+            prev1=curr; // prev1=1;
+
+            // jab next num ke liye chalega 
+            // curr=max(1,0+2)=2;
+            //prev2=1;
+            //prev1=2;
+
+            // next num-> 3
+            // curr=max(2,1+3)=4
+            //prev2=2
+            //prev1=4;
+
+            // next num->1
+            // curr=max(4,2+1)=4
+            // prev2=4
+            // prev1=curr=4
+            // ouptut -> 4
         }
         return prev1;
     }
