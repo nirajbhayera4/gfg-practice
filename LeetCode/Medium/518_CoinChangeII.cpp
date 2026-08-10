2cd8e14b521a07a4adeb49e8200cfa41450c2cb8
NIRAJ <nirajbhayera4@gmail.com>
2026-08-10 20:04:49 +0530

    Sync LeetCode - 518. Coin Change II (Medium)

---
+++ LeetCode/Medium/518_CoinChangeII.cpp
@@ -0,0 +1,41 @@
+/**
+ * Problem Link : https://leetcode.com/problems/coin-change-ii/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+int n;
+int t[301][5001];
+int solve(int i, vector<int> &coins, int amount){
+    if(amount==0){
+        return 1;
+    }
+    if(i==n){
+        return 0;
+    }
+    if(t[i][amount]!=-1){
+        return t[i][amount];
+    }
+    if(coins[i] >amount){
+        return t[i][amount]=solve(i+1, coins, amount);
+    }
+    int take=solve(i, coins, amount - coins[i]);
+    int skip=solve(i+1, coins, amount);
+    return t[i][amount]=take + skip;
+
+}
+    int change(int amount, vector<int>& coins) {
+        n=coins.size();
+        memset(t,-1,sizeof(t));
+        return solve(0,coins, amount);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
