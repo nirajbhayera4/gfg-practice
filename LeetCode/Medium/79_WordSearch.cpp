410dc873e3e6247085a26092483b46995bbbf11b
NIRAJ <nirajbhayera4@gmail.com>
2026-08-14 11:22:10 +0530

    Sync LeetCode - 79. Word Search (Medium)

--- LeetCode/Medium/79_WordSearch.cpp
+++ LeetCode/Medium/79_WordSearch.cpp
@@ -9,42 +9,42 @@ using namespace std;
 
 class Solution {
 public:
-int n, m;
-vector<vector<int>> directions={{1,0},{-1,0},{0,1},{0,-1}};
-bool find(vector<vector<char>>& board,int i, int j, int idx, string &word){
-    if(idx==word.length())return true;// idx agar last element tak pahuch gya hai to true krdo means element mil chuka h
-    if(i <0 || j<0 || j>=m || i>=n || board[i][j]=='$'){
+    int n, m;
+    vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    bool find(vector<vector<char>>& grid, int i, int j, int idx, string& word) {
+        if (idx == word.length()) {
+            return true; // means idx last word tak pahuch gya hai
+        }
+
+        if(i <0 || j<0 || i>=n || j>=m || grid[i][j]=='$'){
+            return false;
+        }
+        if(grid[i][j]!=word[idx])return false;
+
+        char temp=grid[i][j];
+        grid[i][j]='$';
+
+        for(auto &dir : directions){
+            int i_=i + dir[0];
+            int j_=j + dir[1];
+            if(find(grid, i_, j_, idx+1, word))return true;
+        }
+        // backtrack krke value wapis assign krdo 
+        grid[i][j]=temp;
         return false;
     }
 
-    if(board[i][j]!=word[idx])return false; // means element ka index board of i and j ke equal nahi hai 
-    char temp=board[i][j];
-    board[i][j]='$';
-
-    for(auto &dir : directions){
-        int i_=i + dir[0];
-        int j_=j+ dir[1];
-
-        if(find(board, i_, j_, idx+1,word)){
-            return true;
-        }
-
-    }
-    board[i][j]=temp;
-    return false;
-}
-    bool exist(vector<vector<char>>& board, string word) {
-        n=board.size();
-        m=board[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(board[i][j]==word[0] && find(board,i, j, 0,word)){
+    bool exist(vector<vector<char>>& grid, string word) {
+        n = grid.size();
+        m = grid[0].size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == word[0] && find(grid, i, j, 0, word)) {
                     return true;
                 }
             }
         }
         return false;
-
     }
 };
 
