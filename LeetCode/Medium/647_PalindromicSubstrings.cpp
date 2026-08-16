560bde39065a1105bfc7a620a3088ca827afb5db
NIRAJ <nirajbhayera4@gmail.com>
2026-08-16 10:55:55 +0530

    Sync LeetCode - 647. Palindromic Substrings (Medium)

--- LeetCode/Medium/647_PalindromicSubstrings.cpp
+++ LeetCode/Medium/647_PalindromicSubstrings.cpp
@@ -9,34 +9,33 @@ using namespace std;
 
 class Solution {
 public:
-    int t[1001][1001];
-
-    bool check(string &s, int i, int j) {
-        if (i >=j) {
-            return true;
-        }
-        if (t[i][j] != -1) {
-            return t[i][j];
-        }
-        // simple logic hai---> palindrome ke liye first and last index ko
-        // inward move kro
-        if (s[i] == s[j]) {
-            return t[i][j] = check(s, i + 1, j - 1);
-        }
-        return t[i][j]=false;
+int t[1001][100];
+bool check(string &s, int i, int j){
+    if(i >j){
+        return true;
     }
+    if(t[i][j]!=-1){
+        return t[i][j];
+    }
+
+    if(s[i]==s[j]){
+        return t[i][j]=check(s,i+1,j-1);
+    }
+    return false;
+}
     int countSubstrings(string s) {
-        int n = s.size();
-        memset(t, -1, sizeof(t));
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (check(s, i, j)) {
+        int n=s.size();
+        int count=0;
+        memset(t,-1,sizeof(t));
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(check(s,i,j)){
                     count++;
                 }
             }
         }
         return count;
+        
     }
 };
 
