0818b38ae298b64582df46497080449816b2f2d7
NIRAJ <nirajbhayera4@gmail.com>
2026-08-26 10:22:01 +0530

    Sync LeetCode - 27. Remove Element (Easy)

---
+++ LeetCode/Easy/27_RemoveElement.cpp
@@ -0,0 +1,30 @@
+/**
+ * Problem Link : https://leetcode.com/problems/remove-element/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int i = 0;
+
+        for(int j = 0; j < nums.size(); j++) {
+
+            if(nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
