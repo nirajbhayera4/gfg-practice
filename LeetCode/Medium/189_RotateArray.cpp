39cf6243d4abf9e6bd66701f3f8ca212a9a0f6bc
NIRAJ <nirajbhayera4@gmail.com>
2026-08-26 10:36:18 +0530

    Sync LeetCode - 189. Rotate Array (Medium)

---
+++ LeetCode/Medium/189_RotateArray.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Link : https://leetcode.com/problems/rotate-array/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        k = k % n;
+
+        reverse(nums.begin(), nums.end());
+
+        reverse(nums.begin(), nums.begin() + k);
+
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
