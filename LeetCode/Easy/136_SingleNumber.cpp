9d39107d2a2c5af79ff5c3f96d200a26968815ce
NIRAJ <nirajbhayera4@gmail.com>
2026-08-28 19:49:27 +0530

    Sync LeetCode - 136. Single Number (Easy)

---
+++ LeetCode/Easy/136_SingleNumber.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Link : https://leetcode.com/problems/single-number/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(int val : nums)
+        {
+            ans = ans ^ val;
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
