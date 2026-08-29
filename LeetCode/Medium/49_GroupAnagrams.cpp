06ee2c2b8ba72a7b20005145df70ef82a5e2facd
NIRAJ <nirajbhayera4@gmail.com>
2026-08-29 23:00:35 +0530

    Sync LeetCode - 49. Group Anagrams (Medium)

---
+++ LeetCode/Medium/49_GroupAnagrams.cpp
@@ -0,0 +1,32 @@
+/**
+ * Problem Link : https://leetcode.com/problems/group-anagrams/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>>mp;
+        for(string s : strs){
+            string key=s;
+            sort(key.begin(), key.end());
+
+            mp[key].push_back(s);
+
+        }
+        vector<vector<string>> ans;
+        //map se iterate kr lenge 
+        for(auto &it : mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
