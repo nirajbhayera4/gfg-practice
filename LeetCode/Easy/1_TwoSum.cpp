eb603144e13b0131667ffc6ee678c63dfab96519
NIRAJ <nirajbhayera4@gmail.com>
2026-08-29 22:29:25 +0530

    Sync LeetCode - 1. Two Sum (Easy)

--- LeetCode/Easy/1_TwoSum.cpp
+++ LeetCode/Easy/1_TwoSum.cpp
@@ -10,6 +10,7 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        /*
         int n=nums.size();
         for(int i=0;i<n;i++){
             for(int j=i +1;j<n;j++){
@@ -17,6 +18,17 @@ public:
                     return {i,j};
                 }
             }
+        }
+        return {};
+        */
+        unordered_map<int, int> mp;
+        for(int i=0;i<nums.size();i++){
+            int needed=target - nums[i];
+            if(mp.find(needed)!=mp.end()){
+                return {mp[needed],i };
+
+            }
+            mp[nums[i]]=i;
         }
         return {};
     }
