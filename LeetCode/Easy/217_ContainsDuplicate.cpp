c27598f72c468246ec66a09d8a32bb4176b5a8f5
NIRAJ <nirajbhayera4@gmail.com>
2026-08-30 23:06:13 +0530

    Sync LeetCode - 217. Contains Duplicate (Easy)

---
+++ LeetCode/Easy/217_ContainsDuplicate.cpp
@@ -0,0 +1,30 @@
+/**
+ * Problem Link : https://leetcode.com/problems/contains-duplicate/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for(int num : nums){
+            freq[num]++;
+        }
+
+        for(auto &it  : freq)
+        {
+            if(it.second==2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
